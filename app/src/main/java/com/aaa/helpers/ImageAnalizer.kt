512c1467849100f54652e90916349acbc14ea6b5
DimaPuz2010 <91826731+DimaPuz2010@users.noreply.github.com>
2023-05-21 17:57:24 +0600

    6

--- app/src/main/java/com/aaa/helpers/ImageAnalizer.kt
+++ app/src/main/java/com/aaa/helpers/ImageAnalizer.kt
@@ -1,12 +1,33 @@
 package com.aaa.helpers
 
+import android.annotation.SuppressLint
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.pose.PoseDetection
+import com.google.mlkit.vision.pose.PoseLandmark
+import com.google.mlkit.vision.pose.accurate.AccuratePoseDetectorOptions
 
 class ImageAnalizer : ImageAnalysis.Analyzer {
+    private val options = AccuratePoseDetectorOptions
+        .Builder()
+        .setDetectorMode(AccuratePoseDetectorOptions.STREAM_MODE)
+        .build()
+    private val detector = PoseDetection.getClient(options)
+    @SuppressLint("UnsafeOptInUsageError")
     override fun analyze(image: ImageProxy) {
-        println("dfd")
-
-        image.close()
+        val frameImage = image.image
+        if (frameImage != null)
+        {
+            val imageForDetectior = InputImage.fromMediaImage(frameImage, image.imageInfo.rotationDegrees)
+            val task = detector.process(imageForDetectior)
+                task.addOnSuccessListener {
+                    it.getPoseLandmark(PoseLandmark.RIGHT_ELBOW)
+                    image.close()
+                }
+                .addOnFailureListener() {
+                    image.close()
+                }
+        }
     }
 }
